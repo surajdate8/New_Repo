efde368a5f648f9c636df49b7c6c9eb157a0b6b1
suraj <suraj@suraj-Lenovo-G580>
2019-08-15 21:45:21 +0530

    Updated Of 15th August

--- src/test/java/SeleniumFirstTest.java
+++ src/test/java/SeleniumFirstTest.java
@@ -7,6 +7,7 @@ public class SeleniumFirstTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Comment
 		
 		System.out.println("Testing started");
 		WebDriverManager.firefoxdriver().setup();
